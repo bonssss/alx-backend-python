e14243011107a243baa20d87f8b43c4c808c9e94
unknown <bons6710hos@gmail.com>
2025-07-23 14:08:13 +0300

    Logging User Requests(Basic Middleware)

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'chats.middleware.RequestLoggingMiddleware',  # Custom middleware for logging requests
+    'chats.middleware.RequestLoggingMiddleware',  
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
