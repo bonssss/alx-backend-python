63250bbe708092277dfc7d0673eef7ff389a055d
bons <bons6710hos@gmail.com>
2025-07-25 19:43:33 +0300

    Restrict Chat Access by time

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -53,8 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware', 
     'chats.middleware.RequestLoggingMiddleware',  
-    'chats.middleware.RestrictAccessByTimeMiddleware',  
+     
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
